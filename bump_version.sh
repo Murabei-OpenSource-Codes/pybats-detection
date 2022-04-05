d9cf464c26096f65bcbd188023fa012a8a335368
Andre Menezes <a.menezes@murabei.com>
2022-04-05 16:02:28 -0300

    :bookmark: update the bumped sh script

--- bump_version.sh
+++ bump_version.sh
@@ -1,10 +1,16 @@
+nosetests -v ../tests/test__smooth.py
+nosetests -v ../tests/test__intervention.py
+nosetests -v ../tests/test__monitor.py
+
+
 source VERSION
 sed -e 's#{VERSION}#'"${VERSION}"'#g' setup_template.cfg > setup.cfg
 
+rm -R dist
 rm -R build
 python3 setup.py build sdist bdist_wheel
-git add --all
-git commit -m "Bumped version number to ${VERSION}"
-git tag -a ${VERSION} -m "Bumped version number to ${VERSION}"
-git push
-git push origin ${VERSION}
+# git add --all
+# git commit -m "Bumped version number to ${VERSION}"
+# git tag -a ${VERSION} -m "Bumped version number to ${VERSION}"
+# git push
+# git push origin ${VERSION}
