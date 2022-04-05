91740d1bdb638a75a53637cc4efaf3d9a1ea64ca
Andre Menezes <a.menezes@murabei.com>
2022-04-05 16:04:39 -0300

    :white_check_mark: update unit tests for intervention with regression

--- tests/test__intervention.py
+++ tests/test__intervention.py
@@ -112,3 +112,36 @@ class TestIntervention(unittest.TestCase):
             y=df_simulated["y"], interventions=list_interventions)
         self.assertEqual(list(out.keys()), ["filter", "smooth", "model"])
         self.assertTrue(np.all(out["smooth"]["posterior"]["variance"] > 0))
+
+    def test__subjective_intervention_with_regression(self):
+        """Test subjective intervention with regressor."""
+        # Generating level data model
+        np.random.seed(66)
+        X = np.random.normal(0, .1, 100).reshape(-1, 1)
+        rdlm = RandomDLM(n=100, V=.1, W=[0.006, .001])
+        df_simulated = rdlm.level_with_covariates(
+            start_level=100, start_covariates=[-2], X=X,
+            dict_shift={"t": [30], "mean_shift": [10], "var_shift": [1]})
+
+        # Define model
+        a0 = np.array([100, 0, -1])
+        R0 = np.eye(3)
+        R0[0, 0] = 100
+        R0[2, 2] = 10
+
+        mod = dlm(a0=a0, R0=R0, n0=1, s0=.1, ntrend=2, nregn=1,
+                  delregn=.98, deltrend=0.95)
+
+        # List with the interventions
+        list_interventions = [{
+            "time_index": 31, "which": ["noise"],
+            "parameters": [
+                {"h_shift": np.array([110, 0, 0]),
+                 "H_shift": np.eye(3)*0.0}]}]
+
+        dlm_intervention = Intervention(mod=mod)
+        out = dlm_intervention.fit(
+            y=df_simulated["y"], X=df_simulated[["x1"]],
+            interventions=list_interventions)
+        self.assertEqual(list(out.keys()), ["filter", "smooth", "model"])
+        self.assertTrue(np.all(out["smooth"]["posterior"]["variance"] > 0))
