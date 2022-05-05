203335e6c8b1f034c52da8b8a1a1aafac09c3d50
Andre Menezes <a.menezes@murabei.com>
2022-05-05 09:42:12 -0300

    :rocket: improve sh script to bump new version

--- bump_version.sh
+++ bump_version.sh
@@ -1,7 +1,9 @@
+cd src
 nosetests -v ../tests/test__smooth.py
 nosetests -v ../tests/test__intervention.py
 nosetests -v ../tests/test__monitor.py
 
+cd ..
 
 source VERSION
 sed -e 's#{VERSION}#'"${VERSION}"'#g' setup_template.cfg > setup.cfg
@@ -9,6 +11,7 @@ sed -e 's#{VERSION}#'"${VERSION}"'#g' setup_template.cfg > setup.cfg
 rm -R dist
 rm -R build
 python3 setup.py build sdist bdist_wheel
+
 # git add --all
 # git commit -m "Bumped version number to ${VERSION}"
 # git tag -a ${VERSION} -m "Bumped version number to ${VERSION}"
